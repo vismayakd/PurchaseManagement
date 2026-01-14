b764310be8a7b1c25076cb3e1414a43665d7942c
vismayakd <vismayakd1993@gmail.com>
2026-01-14 13:32:37 +0530

    added command for creating superuser

--- start.sh
+++ start.sh
@@ -2,8 +2,16 @@
 # Exit on error
 set -o errexit
 
-# Run migrations automatically
+# Run migrations
 python manage.py migrate
 
-# Start Gunicorn using the PORT assigned by Render
+# Create a superuser automatically (only if variables are set)
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python manage.py createsuperuser \
+        --no-input \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL || true
+fi
+
+# Start Gunicorn
 gunicorn Purchaseproject.wsgi:application --bind 0.0.0.0:${PORT:-8000}
